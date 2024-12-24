1d22873d1f9923e3c3659cac1f66d018f8ec90a2
Luiz Fonseca <lf.fonseca@hotmail.com>
2024-12-24 13:36:55 -0300

    Adding percentage of collisions

--- examples/veins/ncollisions.py
+++ examples/veins/ncollisions.py
@@ -3,11 +3,17 @@ METHOD = 'HVCFLThreshold'
 ENVIRONMENT = 'final/8RSU/'
 RESULTS_PATH = 'python/experiments/' + EXPERIMENT + '/' + ENVIRONMENT + METHOD + '/1/results/General-#0.sca'
 NCOLLISIONS = 'ncollisions'
+GENERATEDWSMS = 'generatedWSMs'
 
 ncollisions = 0
+total = 0
 with open(RESULTS_PATH, 'r') as results_file:
     for line in results_file.readlines():
-        if NCOLLISIONS in line:
+        if GENERATEDWSMS in line:
+            total += int(line.split()[3])
+        elif NCOLLISIONS in line:
             ncollisions += int(line.split()[3])
 
 print('Number of Collisions: ', ncollisions)
+print('Total Messages Generated: ', total)
+print('Percentage of Collisions: ', ncollisions / float(total))
