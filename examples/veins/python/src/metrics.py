a1f7a80426be889fcabcefb29bfd6eef2daacdfc
Luiz Fonseca <lf.fonseca@hotmail.com>
2025-01-20 00:01:53 -0300

    Implementing CKA in the GPU

--- examples/veins/python/src/metrics.py
+++ examples/veins/python/src/metrics.py
@@ -1,7 +1,10 @@
+import torch
 import numpy as np
 import tensorflow as tf
 from sklearn.cross_decomposition import CCA
 from sklearn.metrics import balanced_accuracy_score
+
+device = torch.device('cuda')
 
 def flatten(w):
     r = np.array([], dtype=np.float32)
@@ -12,23 +15,32 @@ def flatten(w):
 def cossim(a, b):
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
-def cka(features_x, features_y):
-    features_x = features_x - np.mean(features_x, 0, keepdims=True)
-    features_y = features_y - np.mean(features_y, 0, keepdims=True)
-    dot_product_similarity = np.linalg.norm(features_x.T.dot(features_y)) ** 2
-    normalization_x = np.linalg.norm(features_x.T.dot(features_x))
-    normalization_y = np.linalg.norm(features_y.T.dot(features_y))
-    return dot_product_similarity / (normalization_x * normalization_y)
+def centering(K):
+    n = K.shape[0]
+    unit = torch.ones([n, n], device=device)
+    I = torch.eye(n, device=device)
+    H = I - unit / n
+    return torch.matmul(torch.matmul(H, K), H)
 
-#def cca(features_x, features_y):
-#    qx, _ = np.linalg.qr(features_x)
-#    qy, _ = np.linalg.qr(features_y)
-#    return np.linalg.norm(qx.T.dot(qy)) ** 2 / min(features_x.shape[1], features_y.shape[1])
+def hsic(X, Y):
+    L_X = torch.matmul(X, X.T)
+    L_Y = torch.matmul(Y, Y.T)
+    return torch.sum(centering(L_X) * centering(L_Y))
 
-def cca(features_x, features_y, n_components):
+def cka(X, Y):
+    X = torch.from_numpy(X).cuda()
+    Y = torch.from_numpy(Y).cuda()
+    return (hsic(X, Y) / (torch.sqrt(hsic(X, X)) * torch.sqrt(hsic(Y, Y)))).cpu()
+
+#def cca(X, Y):
+#    qx, _ = np.linalg.qr(X)
+#    qy, _ = np.linalg.qr(Y)
+#    return np.linalg.norm(qx.T.dot(qy)) ** 2 / min(X.shape[1], Y.shape[1])
+
+def cca(X, Y, n_components):
     cca_obj = CCA(n_components=n_components, max_iter=2000)
-    cca_obj.fit(features_x, features_y)
-    x_c, y_c = cca_obj.transform(features_x, features_y)
+    cca_obj.fit(X, Y)
+    x_c, y_c = cca_obj.transform(X, Y)
     return np.mean([np.corrcoef(x_c[:, i], y_c[:, i])[0, 1] for i in range(n_components)])
 
 def balanced_accuracy(model, rmodel, X, y):
